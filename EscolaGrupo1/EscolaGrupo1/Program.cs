b92955a8793c31c33adac3b5d77fc42f3463646a
vitor paulo <virtorpaulo333019@gmail.com>
2022-03-22 15:55:31 -0300

    init

---
+++ EscolaGrupo1/EscolaGrupo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EscolaGrupo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
