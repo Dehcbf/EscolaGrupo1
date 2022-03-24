bb141e40da428a596d79753fb3ebcb7bdc488963
Luana Matos Brito Sales <luanabritoms@gmail.com>
2022-03-24 00:23:44 -0300

    Adicionado entidade, service, repository e database de Notas

---
+++ EscolaGrupo1/EscolaGrupo1/Entities/Nota.cs
@@ -0,0 +1,20 @@
+﻿using EscolaGrupo1.Utils;
+
+namespace EscolaGrupo1.Entities
+{
+    public class Nota : BaseEntity
+    {
+        public string NomeTurma { get; set; }
+        public string Materia { get; set; }
+        public string Aluno { get; set; }
+        public double NotaAluno { get; set; }
+
+        public Nota(string nomeTurma, string materia, string aluno, double notaAluno)
+        {
+            NomeTurma = nomeTurma;
+            Materia = materia;
+            Aluno = aluno;
+            NotaAluno = notaAluno;
+        }
+    }
+}
---
+++ EscolaGrupo1/EscolaGrupo1/Services/NotaService.cs
@@ -0,0 +1,210 @@
+﻿using EscolaGrupo1.Entities;
+using EscolaGrupo1.Repositories;
+using EscolaGrupo1.turmas;
+using System;
+
+namespace EscolaGrupo1.Services
+{
+    public class NotaService
+    {
+        private NotaRepository _notaRepository;
+        private AulaRepository _aulaRepository;
+        private AlunoRepository _alunoRepository;
+
+        public NotaService()
+        {
+            _notaRepository = new NotaRepository();
+        }
+
+        private Turma VerificaTurma()
+        {
+            Console.Write("Digite o nome da turma: ");
+            var identificadorTurma = int.Parse(Console.ReadLine());
+            return _turmaRepository.ProcurarTurma(identificadorTurma);
+        }
+
+        private Aula VerificaAula()
+        {
+            Console.Write("Digite qual a aula: ");
+            var materiaAula = Console.ReadLine();
+            return _aulaRepository.ProcurarAula(materiaAula);
+        }
+
+        private Aluno VerificaAluno()
+        {
+            Console.Write("Digite o nome do aluno: ");
+            var nomeAluno = Console.ReadLine();
+            return _alunoRepository.BuscarAluno(nomeAluno);
+        }
+
+        public bool CadastrarNota()
+        {
+            Console.Clear();
+
+            var resultadoTurma = VerificaTurma();
+            if (resultadoTurma is null)
+            {
+                Console.WriteLine("Turma não encontrado.");
+                return false;
+            }
+
+            var resultadoMateriaAula = VerificaAula();
+            if (resultadoMateriaAula is null)
+            {
+                Console.WriteLine("Aula não encontrada.");
+                return false;
+            }
+
+            var resultadoAluno = VerificaAluno();
+            if (resultadoAluno is null)
+            {
+                Console.WriteLine("Aluno(a) não encontrado.");
+                return false;
+            }
+
+            Console.Write($"Digite a nota do aluno(a) {resultadoAluno.Nome}: ");
+            var notaAluno = double.Parse(Console.ReadLine());
+
+            Nota nota = new(resultadoTurma.Nome, resultadoMateriaAula.NomeMateria, resultadoAluno.Nome, notaAluno);
+            _notaRepository.Criar(nota);
+
+            return true;
+        }
+
+        public bool AtualizarNota()
+        {
+            Console.Clear();
+            var resultadoTurma = VerificaTurma();
+            if (resultadoTurma is null)
+            {
+                Console.WriteLine("Turma não encontrado.");
+                return false;
+            }
+
+            var resultadoMateriaAula = VerificaAula();
+            if (resultadoMateriaAula is null)
+            {
+                Console.WriteLine("Aula não encontrada.");
+                return false;
+            }
+
+            var resultadoAluno = VerificaAluno();
+            if (resultadoAluno is null)
+            {
+                Console.WriteLine("Aluno(a) não encontrado.");
+                return false;
+            }
+
+            var resultadoAlunoNota = _notaRepository.VerNotaPeloNomeAluno(resultadoAluno.Nome);
+            if (resultadoAlunoNota is null)
+            {
+                Console.WriteLine("Aluno não possui notas.");
+                return false;
+            }
+
+            Console.WriteLine("Notas");
+            foreach (var aluno in resultadoAlunoNota)
+            {
+                Console.WriteLine($"{aluno.NotaAluno}");
+            }
+
+            Console.WriteLine("Digite a nota que será atualizada: ");
+            var notaAntiga = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nova nota: ");
+            var notaAtualizada = double.Parse(Console.ReadLine());
+
+            Nota notaDeletada = new(resultadoTurma.Nome, resultadoMateriaAula.NomeMateria, resultadoAluno.Nome, notaAntiga);
+            _notaRepository.Deletar(notaDeletada);
+            Nota notaNova = new(resultadoTurma.Nome, resultadoMateriaAula.NomeMateria, resultadoAluno.Nome, notaAtualizada);
+            _notaRepository.Criar(notaNova);
+
+            return true;
+        }
+
+        public bool DeletarNota()
+        {
+            Console.Clear();
+            var resultadoTurma = VerificaTurma();
+            if (resultadoTurma is null)
+            {
+                Console.WriteLine("Turma não encontrado.");
+                return false;
+            }
+
+            var resultadoMateriaAula = VerificaAula();
+            if (resultadoMateriaAula is null)
+            {
+                Console.WriteLine("Aula não encontrada.");
+                return false;
+            }
+
+            var resultadoAluno = VerificaAluno();
+            if (resultadoAluno is null)
+            {
+                Console.WriteLine("Aluno(a) não encontrado.");
+                return false;
+            }
+
+            var resultadoAlunoNota = _notaRepository.VerNotaPeloNomeAluno(resultadoAluno.Nome);
+            if (resultadoAlunoNota is null)
+            {
+                Console.WriteLine("Aluno não possui notas.");
+                return false;
+            }
+
+            Console.WriteLine("Notas");
+            foreach (var aluno in resultadoAlunoNota)
+            {
+                Console.WriteLine($"{aluno.NotaAluno}");
+            }
+
+            Console.WriteLine("Digite a nota que será deletada: ");
+            var notaDeletada = double.Parse(Console.ReadLine());
+
+            Nota nota = new(resultadoTurma.Nome, resultadoMateriaAula.NomeMateria, resultadoAluno.Nome, notaDeletada);
+            _notaRepository.Deletar(nota);
+
+            return true;
+        }
+
+        public bool VerTodasNotasPorAluno()
+        {
+            Console.Clear();
+            var resultadoTurma = VerificaTurma();
+            if (resultadoTurma is null)
+            {
+                Console.WriteLine("Turma não encontrado.");
+                return false;
+            }
+
+            var resultadoMateriaAula = VerificaAula();
+            if (resultadoMateriaAula is null)
+            {
+                Console.WriteLine("Aula não encontrada.");
+                return false;
+            }
+
+            var resultadoAluno = VerificaAluno();
+            if (resultadoAluno is null)
+            {
+                Console.WriteLine("Aluno(a) não encontrado.");
+                return false;
+            }
+
+            var resultadoAlunoNota = _notaRepository.VerNotaPeloNomeAluno(resultadoAluno.Nome);
+            if (resultadoAlunoNota is null)
+            {
+                Console.WriteLine("Aluno não possui notas.");
+                return false;
+            }
+
+            Console.WriteLine($"Notas do aluno {resultadoAluno.Nome}");
+            foreach (var aluno in resultadoAlunoNota)
+            {
+                Console.WriteLine($"{aluno.NotaAluno}");
+            }
+
+            return true;
+        }
+    }
+}
